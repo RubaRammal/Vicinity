9a3bf4aab5c09e2558902b2a51384b71428e95be
AMAL <Amalokaili@gmail.com>
2015-02-27 20:02:46 +0300

    Message class (fetch msg - load image - save image)

--- app/src/main/java/vicinity/model/Message.java
+++ app/src/main/java/vicinity/model/Message.java
@@ -1,7 +1,20 @@
 package vicinity.model;
 
 import android.content.Context;
+import android.content.ContextWrapper;
+import android.database.Cursor;
+import android.database.DatabaseUtils;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.widget.ImageView;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.sql.SQLException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -9,80 +22,173 @@ import java.util.Date;
 
 public class Message {
     private static final String TAG = "MessageClass";
-    //private final Context getApplicationContext;
-
+    private static Context getApplicationContext = null;
+    DBHandler dbh;
+    SQLiteDatabase db;
     //Message Attributes
     private String sentAt;
     private Friend sender;
     private Friend receiver;
     private String messageBody; //I'll leave it as a String for now.
-    DBHandler dbHandler;/*I'm just trying out some db functions so there might be better ways of
-                          accessing the db from different classes other than this way - Amal */
+
 
 
     //Constructor
-    public Message(Context getApplicationContext, Friend sender, Friend receiver, String messageBody){
-        //this.getApplicationContext = getApplicationContext;
+    public Message(Context getApplicationContext, Friend sender, Friend receiver, String messageBody) {
+        this.getApplicationContext = getApplicationContext;
 
         Date currentDate = new Date();
-        DateFormat dF=  new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        DateFormat dF = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
         sentAt = dF.format(currentDate);
-        this.sender=sender;
-        this.receiver=receiver;
-        this.messageBody=messageBody;
-       // dbHandler = new DBHandler(this.getApplicationContext);
-//amal
+        this.sender = sender;
+        this.receiver = receiver;
+        this.messageBody = messageBody;
+
+
+
+
     }
 
     //Setters & Getters
 
-    public String getDate(){
+    public Context getApplicationContext() {
+        return getApplicationContext();
+    }
+    public String getDate() {
         return this.sentAt;
     }
 
-    public boolean setSender(Friend sender){
-        this.sender=sender;
+    public boolean setSender(Friend sender) {
+        this.sender = sender;
         return true;
     }
-    public User getSender(){
+
+    public User getSender() {
         return this.sender;
     }
 
-    public boolean setReceiver(Friend receiver){
-        this.receiver=receiver;
+    public boolean setReceiver(Friend receiver) {
+        this.receiver = receiver;
         return true;
     }
-    public User getReceiver(){
+
+    public User getReceiver() {
         return this.receiver;
     }
 
-    public boolean setMessageBody(String messageBody){
-        this.messageBody=messageBody;
+    public boolean setMessageBody(String messageBody) {
+        this.messageBody = messageBody;
         return true;
     }
-    public String getMessageBody(){
+
+    public String getMessageBody() {
         return this.messageBody;
     }
 
 
     //Message Methods [to implement after adding the database]
-    public boolean saveImage(){
+    //we might not need this method since i read that android provides the option of saving an image by default
+    private String saveImage(Bitmap bitmapImage){
 
+        ContextWrapper cw = new ContextWrapper(getApplicationContext());
+        // path to /data/data/Vicinity/app_data/imageDir
+        File directory = cw.getDir("imageDir" , Context.MODE_PRIVATE);
+        // Create imageDir
+        File mypath = new File(directory , "profile.jpg");
 
-        return false;
+        FileOutputStream fos = null;
+
+        try {
+            fos = new FileOutputStream(mypath);
+
+            // Use the compress method on the BitMap object to write image to the OutputStream
+            bitmapImage.compress(Bitmap.CompressFormat.PNG, 100, fos);
+
+            try {
+                fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+       return directory.getAbsolutePath();
+
     }
-    public void viewMessage(int messageID)  {
 
 
 
-        //"FetchAll" should be used
-        /*
-        SQLiteDatabase db = dbHandler.getReadableDatabase();
-        Cursor cur = null;
+    private void loadImage(String path){
+        try {
+        File f = new File(path , "profile.jpg");
+        Bitmap b = BitmapFactory.decodeStream(new FileInputStream(f));
+        ImageView img = null;
+           // img = (ImageView) findViewById(android.support.v7.appcompat.R.id.imgPicker);
+           //img.setImageBitmap(b);
 
-        db.close();
-        */
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
     }
+
+    public void viewMessage(int messageID) throws SQLException{
+
+            String Table_Name = "Message";
+            String selectQuery = "SELECT * FROM" + Table_Name + "WHERE _ID=" + messageID;
+
+
+        try {
+            db = dbh.getReadableDatabase();
+            dbh.openDataBase();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        Cursor cursor = db.rawQuery(selectQuery, null);
+            String[] data = null;
+
+            if (cursor.moveToFirst()) {
+                do {
+                    // get  the  data into array,or class variable, instead i'm printing it for now
+                  System.out.print(DatabaseUtils.dumpCursorToString(cursor));
+
+                } while (cursor.moveToNext());
+
+
+            }
+                db.close();
+
+
+
+        /*public String[]  getAppCategorydetail(){
+
+            String Table_Name="name of table";
+
+            String selectQuery = "SELECT  * FROM " + Table_Name;
+            SQLiteDatabase db = this.getReadableDatabase();
+            Cursor cursor = db.rawQuery(selectQuery, null);
+            String[] data = null;
+            if (cursor.moveToFirst()) {
+                do {
+                    // get  the  data into array,or class variable
+                } while (cursor.moveToNext());
+            }
+            db.close();
+            return data;
+        }
+*/
+
+
+        }
+
 
 
 }
+
+
+
