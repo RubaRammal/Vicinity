9e1c630661356e6ce69b28a3f40d3b057ed3480b
AfnanSh <a.y.sherbeeni@gmail.com>
2015-05-17 12:04:45 +0300

    Delete friend alert

--- app/src/main/java/vicinity/vicinity/NeighborListAdapter.java
+++ app/src/main/java/vicinity/vicinity/NeighborListAdapter.java
@@ -131,7 +131,7 @@ public class NeighborListAdapter extends BaseAdapter {
                             public void onClick(DialogInterface dialog, int which) {
                                 Log.i(TAG, "Neighbor status: " + neighbor.getStatus());
                                 //Sending the request through RequestsManager AsyncTask
-                                new RequestsManager().execute(neighbor);
+                                new RequestsManager().execute(neighbor);//
                             }
                         })
                         .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
