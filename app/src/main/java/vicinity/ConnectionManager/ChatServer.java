d979b4b664e2043d763ae3b39f6e9ab4bafa6913
Ruba Rammal <ruba.ramm@gmail.com>
2015-05-18 13:31:29 +0300

    ServiceRequest class added.

--- app/src/main/java/vicinity/ConnectionManager/ChatServer.java
+++ app/src/main/java/vicinity/ConnectionManager/ChatServer.java
@@ -11,6 +11,10 @@ import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import vicinity.Controller.MainController;
 import vicinity.Controller.VicinityNotifications;
@@ -22,79 +26,32 @@ import vicinity.vicinity.ChatActivity;
 public class ChatServer implements Runnable{
 
     private final static String TAG ="ChatServer";
-    private ServerSocket chatSocket;
-    private Socket clientSocket;
-    private LocalBroadcastManager toChat;
-    private ObjectInputStream inputStream;
-    private Intent intent;
-    private MainController controller;
 
-
-
-
-    public ChatServer() throws IOException{
-
-        Log.i(TAG,"Chat thread has started...");
-        chatSocket = new ServerSocket(Globals.CHAT_PORT);
-        toChat = LocalBroadcastManager.getInstance(ConnectAndDiscoverService.ctx);
-        intent = new Intent();
-        controller = new MainController(ConnectAndDiscoverService.ctx);
-
-    }
+    private static ChatServer server;
+    private ServerSocket serverSocket;
+    private ExecutorService executorService = Executors.newFixedThreadPool(20);
 
     @Override
     public void run() {
         try {
-            clientSocket = chatSocket.accept();
+            System.out.println("Starting Server");
+            serverSocket = new ServerSocket(Globals.CHAT_PORT);
 
-        InetAddress serverThreadId = clientSocket.getInetAddress();
-        Log.i(TAG,"Friend : "+serverThreadId+" started a chat");
-        inputStream =  new ObjectInputStream(clientSocket.getInputStream());
-
-        } catch (IOException e) {
+            while(true) {
+                System.out.println("Waiting for request");
+                try {
+                    Socket s = serverSocket.accept();
+                    System.out.println("Processing request");
+                    executorService.submit(new ServiceRequest(s));
+                } catch(IOException ioe) {
+                    System.out.println("Error accepting connection");
+                    ioe.printStackTrace();
+                }
+            }
+        }catch(IOException e) {
+            System.out.println("Error starting Server on "+Globals.CHAT_PORT);
             e.printStackTrace();
         }
-        while (Globals.stopServer) {
-            try {
-
-                VicinityMessage msg = (VicinityMessage)inputStream.readObject();
-                msg.setFrom(clientSocket.getInetAddress().getHostAddress());
-                msg.setIsMyMsg(false);
-                Log.i(TAG, "Received a message from: " + msg.getFriendID());
-                Log.i(TAG,"Message content: "+msg.getMessageBody());
-                Log.i(TAG,"Message IP: "+msg.getFrom());
-                controller.addMessage(msg);
-
-
-
-
-                if(!Globals.chatActive){
-                VicinityNotifications.newMessageNotification(msg);
-                intent.putExtra("MSG", msg);
-                intent.setAction(Intent.ACTION_MAIN);
-                intent.addCategory(Intent.CATEGORY_LAUNCHER);
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                ComponentName cn = new ComponentName(ConnectAndDiscoverService.ctx, ChatActivity.class);
-                intent.setComponent(cn);
-                }
-                else{
-                    Intent intent = new Intent("MESSAGE");
-                    intent.putExtra("NEW_MESSAGE",msg);
-                    toChat.sendBroadcast(intent);
-                }
-
-            } catch (IOException e) {
-                e.printStackTrace();
-                break;
-            }
-            catch (ClassNotFoundException e){
-                e.printStackTrace();
-                break;
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
     }
-
 
 }
---
+++ app/src/main/java/vicinity/ConnectionManager/ServiceRequest.java
@@ -0,0 +1,92 @@
+package vicinity.ConnectionManager;
+
+import android.content.ComponentName;
+import android.content.Intent;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.sql.SQLException;
+
+import vicinity.Controller.MainController;
+import vicinity.Controller.VicinityNotifications;
+import vicinity.model.Globals;
+import vicinity.model.VicinityMessage;
+import vicinity.vicinity.ChatActivity;
+
+/**
+ * Created by macproretina on 5/18/15.
+ */
+public class ServiceRequest implements Runnable {
+    private Socket socket;
+    private final static String TAG = "ServiceRequest";
+    private LocalBroadcastManager toChat;
+    private ObjectInputStream inputStream;
+    private Intent intent;
+    private MainController controller;
+
+    public ServiceRequest(Socket connection) throws IOException {
+        this.socket = connection;
+        toChat = LocalBroadcastManager.getInstance(ConnectAndDiscoverService.ctx);
+        intent = new Intent();
+        controller = new MainController(ConnectAndDiscoverService.ctx);
+    }
+
+    public void run() {
+
+        try {
+            Log.i(TAG, "Chat thread server has started...");
+
+
+            InetAddress serverThreadId = socket.getInetAddress();
+            Log.i(TAG, "Friend : " + serverThreadId + " started a chat");
+            inputStream = new ObjectInputStream(socket.getInputStream());
+
+            while (Globals.stopServer) {
+
+
+                VicinityMessage msg = (VicinityMessage) inputStream.readObject();
+                msg.setFrom(socket.getInetAddress().getHostAddress());
+                msg.setIsMyMsg(false);
+                Log.i(TAG, "Received a message from: " + msg.getFriendID());
+                Log.i(TAG, "Message content: " + msg.getMessageBody());
+                Log.i(TAG, "Message IP: " + msg.getFrom());
+                controller.addMessage(msg);
+
+
+                //VicinityNotifications.newMessageNotification(msg);
+
+                if (!Globals.chatActive) {
+                    VicinityNotifications.newMessageNotification(msg);
+                    intent.putExtra("MSG", msg);
+                    intent.setAction(Intent.ACTION_MAIN);
+                    intent.addCategory(Intent.CATEGORY_LAUNCHER);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    ComponentName cn = new ComponentName(ConnectAndDiscoverService.ctx, ChatActivity.class);
+                    intent.setComponent(cn);
+                } else {
+                    Intent intent = new Intent("MESSAGE");
+                    intent.putExtra("NEW_MESSAGE", msg);
+                    toChat.sendBroadcast(intent);
+                }
+
+            }
+
+            socket.close();
+
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
+
